b7a9b93e6c3bd2529a2c5d20797aa9bda7dacb42
yz <652172515@qq.com>
2018-04-25 18:00:09 +0800

    seven

--- mysite/read_statistics/models.py
+++ mysite/read_statistics/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.fields import exceptions
+from django.utils import timezone
 class ReadNum(models.Model):
     read_num=models.IntegerField(default=0)
     content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)
@@ -16,3 +17,10 @@ class ReadNumExpandMethod:
             return readnum.read_num
         except exceptions.ObjectDoesNotExist:
             return 0
+class ReadDetail(models.Model):
+    read_num=models.IntegerField(default=0)
+    date=models.DateField(default=timezone.now)
+    content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
